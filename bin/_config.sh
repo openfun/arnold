a75ea4d0ba26a8d559a2bfc23149798e19739712
Julien Maupetit <julien@maupetit.net>
2018-04-11 15:09:27 +0200

    👌(utils) do not modify env_file in place
    @sampaccoud and @christophe_chaudier where concerned about modifying an
    environment configuration in-place, so we decided to move dynamical env
    vars generation in the docker run call.

--- bin/_config.sh
+++ bin/_config.sh
@@ -8,10 +8,31 @@ set -eo pipefail
 #
 # environment: target environment file name (e.g. development for
 # env.d/development)
+function _configure_environment() {
+
+    environment=${1:-development}
+    env_file="env.d/${environment}"
+
+    if [[ ! -e $env_file ]]; then
+        echo "Environment file ${env_file} does not exists. You should create it first:"
+        echo ""
+        echo "  $ cp env.d/base env.d/${environment}"
+        echo ""
+        echo "And then edit it with relevant values."
+        exit 1
+    fi
+}
+
+# _docker_run: wrap docker run command
+#
+# usage: _docker_run [options] [ARGS...]
+#
+# options: docker run command options
+# ARGS   : docker run command arguments
 #
 # DISCLAIMER:
 #
-# /!\ DO NOT USE THIS UTILITY TO GENERATE A PRODUCTION ENVIRONMENT /!\
+# /!\ DO NOT USE THIS UTILITY FOR A PRODUCTION ENVIRONMENT /!\
 #
 # This utility function is only meant to get used by developers on their local
 # machine to test various OpenShift configurations or instances.
@@ -30,32 +51,14 @@ set -eo pipefail
 #   - minishift has been already started (see bin/dev script)
 #   - your have already logged in to your minishift instance via the oc login
 #     command
-function _configure_environment() {
-
-    environment=${1:-development}
-    env_file="env.d/${environment}"
-
-    if [[ ! -e $env_file ]]; then
-        echo "Environment file ${env_file} does not exists. You should create it first:"
-        echo ""
-        echo "  $ cp env.d/base env.d/${environment}"
-        echo ""
-        echo "And then edit it with relevant values."
-        exit 1
-    fi
-
-    # We suppose that the local user already logged in to the minishift server
-    sed -i -E "s/(K8S_AUTH_API_KEY=)(.*)/\1$(oc whoami -t)/" $env_file
-    sed -i -E "s/(K8S_AUTH_HOST=)(.*)/\1https:\/\/$(minishift ip):8443/" $env_file
-}
-
-# _docker_run: wrap docker run command
-#
-# usage: _docker_run [options] [ARGS...]
-#
-# options: docker run command options
-# ARGS   : docker run command arguments
 function _docker_run() {
+
     _configure_environment
-    docker run --rm -it --env-file $env_file arnold:$(tr -d '\n' < VERSION) "$@"
+
+    docker run --rm -it \
+        --env-file $env_file \
+        --env K8S_AUTH_API_KEY=$(oc whoami -t) \
+        --env K8S_AUTH_HOST="https://$(minishift ip):8443" \
+        arnold:$(tr -d '\n' < VERSION) \
+        "$@"
 }
