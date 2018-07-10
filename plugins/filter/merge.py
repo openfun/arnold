03668475df17375006bb0c5814a9c0aa24b2391a
Julien Maupetit <julien@maupetit.net>
2018-07-10 12:05:51 +0200

    🐛(plugin) fix merge_with_app filter
    Switch to our own apps merge implementation to avoid duplicates in
    lists.

--- plugins/filter/merge.py
+++ plugins/filter/merge.py
@@ -5,11 +5,13 @@ Merge Jinja filters
 from copy import deepcopy
 
 from ansible.errors import AnsibleFilterError
-from deepmerge import always_merger
 
 
 def merge_with_app(base, new):
-    """Merge data from the "new" application to the "base" application"""
+    """
+        Merge data from the "new" application to the "base" application.
+        Services listed in "new" that do not exist in "base" will be ignored.
+    """
 
     if base is None:
         raise AnsibleFilterError("input base app is empty")
@@ -21,25 +23,35 @@ def merge_with_app(base, new):
         raise AnsibleFilterError("input apps definitions should be 'dict' types")
 
     if base.get("name") is None or new.get("name") is None:
-        raise AnsibleFilterError("input apps should have a name key")
+        raise AnsibleFilterError("input apps should have a 'name' key")
 
     if base["name"] != new["name"]:
         raise AnsibleFilterError("input apps should have the same name")
 
+    if not len(base["name"]):
+        raise AnsibleFilterError("input apps name cannot be empty")
+
     result = deepcopy(base)
 
     # Add or override services metadata
-    for service in result["services"]:
-        new_app_selected_services = filter(
-            lambda s: s.get("name") == service.get("name"), new["services"]
-        )
+    for base_service in result["services"]:
+        new_app_selected_services = [
+            s for s in new["services"] if s.get("name") == base_service.get("name")
+        ]
+
         if len(new_app_selected_services) != 1:
             continue
-        service = always_merger.merge(service, new_app_selected_services[0])
+        new_service = new_app_selected_services[0]
+        for k in ("configs", "templates"):
+            if not new_service.get(k):
+                continue
+            # Use the list(set()) trick to remove duplicated items
+            base_service[k] = sorted(list(set(new_service[k] + base_service[k])))
 
     # Merge volumes (if any)
     if result.get("volumes") and new.get("volumes"):
-        result["volumes"] = always_merger.merge(result["volumes"], new["volumes"])
+        # Use the list(set()) trick to remove duplicated items
+        result["volumes"] = sorted(list(set(new["volumes"] + result["volumes"])))
 
     return result
 
