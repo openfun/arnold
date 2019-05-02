c3d21f81e88f6a8f299052f9b7877abf7a868e11
Julien Maupetit <julien@maupetit.net>
2019-05-02 16:03:30 +0200

    ✨(plugins) add tray support for the apps lookup
    Arnold now fully supports tray package format for its applications.
    We've taken advantage of this new feature implementation to refactor the
    whole plugin to make it simpler and testable.
    
    /me was a bit ashamed by the old freaking code...

--- lookup_plugins/apps.py
+++ lookup_plugins/apps.py
@@ -5,8 +5,7 @@ Applications lookup
 # pylint: disable=bad-continuation
 from __future__ import absolute_import, division, print_function
 
-import os
-import re
+from pathlib import Path
 
 import yaml
 from ansible.plugins.lookup import LookupBase
@@ -26,33 +25,31 @@ DOCUMENTATION = """
         "apps" ``apps_path``:
 
             apps/richie
-                ├── templates
-                │   ├── elasticsearch
-                │   │   ├── dc.yml.j2
-                │   │   └── svc.yml.j2
-                │   ├── nginx
-                │   │   ├── _configs
-                │   │   │   └── richie.conf.j2
-                │   │   ├── dc.yml.j2
-                │   │   └── svc.yml.j2
-                │   ├── postgresql
-                │   │   ├── dc.yml.j2
-                │   │   ├── ep.yml.j2
-                │   │   └── svc.yml.j2
-                │   ├── richie
-                │   │   ├── dc.yml.j2
-                │   │   ├── job_collectstatic.yml.j2
-                │   │   ├── job_db_migrate.yml.j2
-                │   │   ├── job_regenerate_indexes.yml.j2
-                │   │   └── svc.yml.j2
-                │   └── _volumes
-                │       ├── media.yml.j2
-                │       └── static.yml.j2
-                └── vars
-                    └── all
-                        └── main.yml
+            ├── templates
+            │   ├── services
+            │   │   ├── app
+            │   │   │   ├── *.yml.j2
+            │   │   ├── elasticsearch
+            │   │   │   └── *.yml.j2
+            │   │   ├── nginx
+            │   │   │   ├── configs
+            │   │   │   │   └── richie.conf.j2
+            │   │   │   └── *.yml.j2
+            │   │   └── postgresql
+            │   │       └── *.yml.j2
+            │   └── volumes
+            │       ├── media.yml.j2
+            │       └── static.yml.j2
+            ├── tray.yml
+            └── vars
+                ├── all
+                │   └── main.yml
+                ├── settings.yml
+                └── vault
+                    └── main.yml.j2
 
-        Analysing this tree with ``lookup_apps`` should return the
+
+        Analysing this tree with the ``apps`` lookup should return the
         following data structure:
 
             [
@@ -67,24 +64,30 @@ DOCUMENTATION = """
                             "templates": [
                                 "apps/richie/templates/nginx/dc.yml.j2",
                                 "apps/richie/templates/nginx/svc.yml.j2"
-                            ]
+                            ],
+                            "environment_variables": None
                         },
                         {
+                            "configs": [],
                             "name": "postgresql",
                             "templates": [
                                 "apps/richie/templates/postgresql/dc.yml.j2",
                                 "apps/richie/templates/postgresql/ep.yml.j2",
                                 "apps/richie/templates/postgresql/svc.yml.j2"
-                            ]
+                            ],
+                            "environment_variables": None
                         },
                         {
+                            "configs": [],
                             "name": "elasticsearch",
                             "templates": [
                                 "apps/richie/templates/elasticsearch/dc.yml.j2",
                                 "apps/richie/templates/elasticsearch/svc.yml.j2"
-                            ]
+                            ],
+                            "environment_variables": None
                         },
                         {
+                            "configs": [],
                             "name": "richie",
                             "templates": [
                                 "apps/richie/templates/richie/job_regenerate_indexes.yml.j2",
@@ -93,7 +96,8 @@ DOCUMENTATION = """
                                 "apps/richie/templates/richie/job_collectstatic.yml.j2",
                                 "apps/richie/templates/richie/svc.yml.j2",
                                 "apps/richie/templates/richie/job_db_migrate.yml.j2"
-                            ]
+                            ],
+                            "environment_variables": None
                         }
                     ],
                     "volumes": [
@@ -106,17 +110,23 @@ DOCUMENTATION = """
                             "name": "main",
                             "path": "apps/richie/vars/all/main.yml",
                         },
-                    ]
+                    ],
+                    settings: {
+                        "databases": [
+                            "engine": "postgresql",
+                            "release": "9.6"
+                        ]
+                    }
                 }
             ]
     options:
-      _apps_paths:
-        description: Paths to look for applications
+      _terms:
+        description: list of application paths to look for
         required: True
 """
 
 EXAMPLES = """
-- debug: msg="Available apps: {{ lookup('apps','apps') }}"
+- debug: msg="Available apps: {{ lookup('apps', apps_path) }}"
 """
 
 RETURN = """
@@ -127,118 +137,124 @@ RETURN = """
 """
 
 
+# pylint: disable=too-many-instance-attributes
 class LookupModule(LookupBase):
     """Apps lookup module"""
 
-    # This function needs a serious refactoring, Pylint is right and it makes no
-    # sense to disable all the following warnings.
-    #
-    # pylint: disable=arguments-differ,too-many-locals,too-many-nested-blocks,too-many-branches,
-    def run(self, terms, variables=None, **kwargs):
+    def __init__(self, loader=None, templar=None, **kwargs):
+        """Add application related properties."""
+        super().__init__(loader=None, templar=None, **kwargs)
 
-        app_volumes_dir = "_volumes"
-        service_config_dir = "_configs"
-        settings_file = "settings.yml"
-        environment_variables_file = "_env.yml.j2"
+        self.configs_dir = "configs"
+        self.services_dir = "services"
+        self.templates_dir = "templates"
+        self.vars_dir = "vars"
+        self.volumes_dir = "volumes"
+        self.env_file = "_env.yml.j2"
+        self.settings_file = "settings.yml"
+        self.tray_file = "tray.yml"
+        # For now we only accept "all" type vars, but this can evolve soon.
+        self.var_types = ("all",)
+        self.apps = []
+
+    @staticmethod
+    def _yaml_load(file_path):
+        """Syntactic sugar to get yaml content with a simpler line of code"""
+
+        return yaml.load(Path(file_path).read_text(), Loader=yaml.FullLoader)
+
+    def _get_app_service(self, service_path):
+        """Explore an application service"""
+
+        p = Path(service_path)
+
+        # Expected environment variables for this service
+        env_vars = p / self.env_file
+        return {
+            "name": p.name,
+            "templates": sorted(map(str, p.glob("[!_]*.yml.j2"))),
+            "configs": sorted(map(str, (p / self.configs_dir).glob("*.j2"))),
+            "environment_variables": str(env_vars) if env_vars.exists() else None,
+        }
+
+    def _get_app_services(self, app_path):
+        """Explore an application services"""
+
+        services = []
+        for service_path in (
+            Path(app_path) / self.templates_dir / self.services_dir
+        ).iterdir():
+            # Each directory store templates for a service
+            if not service_path.is_dir():
+                continue
+            services.append(self._get_app_service(service_path))
+        return services
+
+    def _get_app_settings(self, app_path):
+        """Explore application settings"""
+
+        settings_path = Path(app_path) / self.vars_dir / self.settings_file
+        if not settings_path.exists():
+            return {}
+        return self._yaml_load(settings_path)
+
+    def _get_app_vars(self, app_path):
+        """Get application variables"""
+
+        _vars = []
+        for var_type in self.var_types:
+            for var_file in (Path(app_path) / self.vars_dir / var_type).glob("*.yml"):
+                _vars.append(
+                    {"type": var_type, "name": var_file.stem, "path": str(var_file)}
+                )
+        return _vars
+
+    def _get_app_volumes(self, app_path):
+        """Get application volumes"""
+
+        return sorted(
+            map(
+                str,
+                (Path(app_path) / self.templates_dir / self.volumes_dir).glob(
+                    "*.yml.j2"
+                ),
+            )
+        )
+
+    # pylint: disable=inconsistent-return-statements
+    def _get_app(self, app_path):
+        """Explore an application path and add it to apps if it's a valid app"""
+
+        tray_file = Path(app_path) / self.tray_file
+        # Application is not a valid tray, ignore it
+        if not tray_file.exists():
+            return
+        # Parse the tray file
+        tray = self._yaml_load(tray_file)
+
+        return {
+            "name": tray.get("metadata").get("name"),
+            "services": self._get_app_services(app_path),
+            "vars": self._get_app_vars(app_path),
+            "volumes": self._get_app_volumes(app_path),
+            "settings": self._get_app_settings(app_path),
+        }
+
+    def _get_apps(self, apps_path):
+        """Explore an application path and return discovered apps list."""
+
         apps = []
-        app = None
+        for app_path in Path(apps_path).iterdir():
+            if not app_path.is_dir():
+                continue
+            apps.append(self._get_app(app_path))
+        return apps
+
+    def run(self, terms, variables=None, **kwargs):
+        """This is the lookup plugin entry point that will be invoked"""
 
         for term in terms:
             for apps_path in term:
+                self.apps += self._get_apps(apps_path)
 
-                ignore_dirs_regex = r"^{}/_.*(/.*)*$".format(apps_path.strip("/"))
-
-                for root, _, files in os.walk(apps_path):
-
-                    (head, tail) = os.path.split(root)
-
-                    # ./apps/
-                    if not tail:
-                        continue
-
-                    # Ignore directories matching ignore_dirs_regex
-                    if re.match(ignore_dirs_regex, root):
-                        continue
-
-                    # ./apps/foo app directory
-                    if root == os.path.join(apps_path, tail):
-                        if app is not None:
-                            apps.append(app)
-                        app = {
-                            "name": tail,
-                            "services": [],
-                            "vars": [],
-                            "settings": {},
-                            "environment_variables": None,
-                        }
-                        continue
-
-                    # ./apps/foo/_volumes directory
-                    if tail == app_volumes_dir:
-                        volumes = [os.path.join(root, f) for f in files]
-                        app["volumes"] = volumes
-                        continue
-
-                    # ./apps/foo/templates/bar service templates
-                    if app is not None and root == os.path.join(
-                        apps_path, app["name"], "templates", tail
-                    ):
-                        templates = []
-                        environment_variables = None
-                        for f in files:
-                            if f == environment_variables_file:
-                                environment_variables = os.path.join(root, f)
-                            else:
-                                templates.append(os.path.join(root, f))
-
-                        app["services"].append(
-                            {
-                                "name": tail,
-                                "templates": templates,
-                                "environment_variables": environment_variables,
-                            }
-                        )
-                        continue
-
-                    # ./apps/foo/templates/bar/_configs directory
-                    if tail == service_config_dir:
-                        service = os.path.basename(head)
-                        idx = [s.get("name") for s in app["services"]].index(service)
-                        configs = [os.path.join(root, f) for f in files]
-                        app["services"][idx].update({"configs": configs})
-                        continue
-
-                    # ./apps/foo/vars/settings.yml
-                    if (
-                        root == os.path.join(apps_path, app["name"], "vars")
-                        and settings_file in files
-                    ):
-                        with open(os.path.join(root, settings_file), "r") as stream:
-                            app["settings"].update(
-                                yaml.load(stream, Loader=yaml.FullLoader)
-                            )
-                            continue
-
-                    # ./apps/foo/vars/all
-                    #
-                    # For now we only accept "all" type vars, but this can evolve soon.
-                    for var_type in ("all",):
-                        if root == os.path.join(
-                            apps_path, app["name"], "vars", var_type
-                        ):
-                            for file_ in files:
-                                name, ext = os.path.splitext(file_)
-                                # We only consider YAML files
-                                if ext != ".yml":
-                                    continue
-                                app["vars"].append(
-                                    {
-                                        "type": var_type,
-                                        "name": name,
-                                        "path": os.path.join(root, file_),
-                                    }
-                                )
-
-                apps.append(app)
-
-        return apps
+        return self.apps
